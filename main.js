c846a0581bcdb6852faf6c3744a5b5754e91de3d
Cyril Ledru <cyril.ledru@gmail.com>
2013-02-19 06:41:07 +0900

    Start versioning

---
+++ main.js
@@ -0,0 +1,40 @@
+$(function() {
+	var video = document.getElementById("video1");
+	
+	$( "#seeker" ).slider({
+		value: 0,
+		disabled: true,
+		slide: function( event, ui ) {
+			var video = document.getElementById("video1");
+			video.currentTime = ui.value;	
+		}
+	});
+
+	video.addEventListener('durationchange', function() {
+		$("#seeker").slider("option", "disabled", false);
+		$("#seeker").slider("option", "min", 0);
+		$("#seeker").slider("option", "max", video.duration);
+	});
+
+	$( "#speed" ).slider({
+		orientation: "vertical",
+		range: "min",
+		min: 0,
+		max: 3,
+		step: 0.5,
+		value: 1,
+		slide: function( event, ui ) {
+			var video = document.getElementById("video1");
+			video.playbackRate = ui.value;
+		}
+	});
+
+	$("#play").click(function() {
+		var video = document.getElementById("video1");
+		if (video.paused) {
+			video.play();
+		} else {
+			video.pause();
+		}
+	});
+});
