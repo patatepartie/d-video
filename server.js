04ef05c30cece8e47cc8a6c010f038a9e533e5ac
Cyril Ledru <cyril.ledru@gmail.com>
2013-02-24 22:52:52 +0900

    Switch to Express
    To handle any request.

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var connect = require('connect'), 
-	http = require('http'), 
+var express = require('express'), 
 	app;
 
-app = connect()
-	.use(connect.static('app'))
-	.use('/videos/', connect.static('videos'))
-	.use('/js/lib/', connect.static('node_modules/requirejs'))
-	.use('/node_modules', connect.static('node_modules'));
+app = express.createServer();
 
-http.createServer(app).listen(8880, function() {
-	console.log('Running on http://localhost:8880');
-});
+app.use(express.static('app'));
+app.use('/videos/', express.static('videos'));
+app.use('/js/lib/', express.static('node_modules/requirejs'));
+app.use('/node_modules', express.static('node_modules'));
+
+app.listen(8880);
+
+console.log('Running on http://localhost:8880');
