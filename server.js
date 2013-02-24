c97184123bdbbe0bbfd615c8ef84c9ea59e8aa9d
Cyril Ledru <cyril.ledru@gmail.com>
2013-02-24 11:29:42 +0900

    Use requirejs via node
    For now, need to link requirejs manually between node_modules and lib.
    Grunt will do the work later.

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var connect = require('connect'),
 
 app = connect()
 	.use(connect.static('app'))
-	.use('/lib/', connect.static('lib'));
+	.use('/js/lib/', connect.static('lib'))
+	.use('/node_modules', connect.static('node_modules'));
 
 http.createServer(app).listen(8880, function() {
 	console.log('Running on http://localhost:8880');
