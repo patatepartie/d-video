82066e5415e11cae5771b6f77cb8e6be1d785c29
Cyril Ledru <cyril.ledru@gmail.com>
2013-08-24 12:30:11 +0900

    Use Backbone object definition style on Library

--- public/js/controllers/library.js
+++ public/js/controllers/library.js
@@ -7,8 +7,18 @@ define([
   'models/medium'], 
 
   function(Backbone, Region, MediumSelectionView, MediumEditionView, MediumDeletionView, Media, Medium) {
-    var Library = function() {
-      Backbone.on( {
+    var Library = function(options) {
+      this._configure(options || {});
+      this.initialize.apply(this, arguments);
+    };
+
+    _.extend(Library.prototype, {
+      _configure: function (options) {
+        this.el = options.el;
+      },
+
+      initialize: function () {
+        Backbone.on( {
           "library:select_medium": this.selectMedium,
           "library:edit_medium": this.showEditionView,
           "library:delete_medium": this.showDeletionView,
@@ -18,64 +28,65 @@ define([
           "library:medium_deletion_cancelled": this.cancelMediaDeletion
         }, this);
 
-      this.region = new Region({el: "#medium .selection"});
+        this.region = new Region({el: "#medium .selection"});
 
-      // Replace that by a bootstrap
-      this.media = new Media();
-      this.media.fetch({reset :true});
-    };
+        // Replace that by a bootstrap
+        this.media = new Media();
+        this.media.fetch({reset :true});
+      },
 
-    Library.prototype.selectMedium = function(medium) {
-      this.selectedMedium = medium;
-    };
+      selectMedium: function(medium) {
+        this.selectedMedium = medium;
+      },
 
-    Library.prototype.showEditionView = function (currentMedium) {
-      var medium = currentMedium || new Medium();
+      showEditionView: function(currentMedium) {
+        var medium = currentMedium || new Medium();
 
-      this.region.show(new MediumEditionView({model: medium}));
-    },
+        this.region.show(new MediumEditionView({model: medium}));
+      },
 
-    Library.prototype.validateMediaEditing = function(medium) {
-      var self = this;
+      validateMediaEditing: function(medium) {
+        var self = this;
 
-      if (medium.isNew()) {
-        this.media.add(medium);
+        if (medium.isNew()) {
+          this.media.add(medium);
+        }
+
+        if (medium.hasChanged()) {
+          medium.save().done(function() {
+            self.showMediaView(medium)    
+          });
+        }        
+      },
+
+      cancelMediaEditing: function() {
+        this.showMediaView(this.selectedMedium);
+      },
+
+      showMediaView: function (medium) {
+        var selectionView = new MediumSelectionView({collection: this.media});
+
+        this.region.show(selectionView);
+
+        if (medium) {
+          selectionView.selectMedium(medium);        
+        }
+      },
+
+      showDeletionView: function (currentMedium) {
+        this.region.show(new MediumDeletionView({model: currentMedium}));
+      },
+
+      validateMediaDeletion: function(medium) {
+        medium.destroy();
+
+        this.showMediaView();
+      },
+
+      cancelMediaDeletion: function() {
+        this.showMediaView(this.selectedMedium);
       }
-
-      if (medium.hasChanged()) {
-        medium.save().done(function() {
-          self.showMediaView(medium)    
-        });
-      }        
-    },
-
-    Library.prototype.cancelMediaEditing = function() {
-      this.showMediaView(this.selectedMedium);
-    },
-
-    Library.prototype.showMediaView = function (medium) {
-      var selectionView = new MediumSelectionView({collection: this.media});
-
-      this.region.show(selectionView);
-
-      if (medium) {
-        this.selectionView.selectMedium(medium);        
-      }
-    },
-
-    Library.prototype.showDeletionView = function (currentMedium) {
-      this.region.show(new MediumDeletionView({model: currentMedium}));
-    },
-
-    Library.prototype.validateMediaDeletion = function(medium) {
-      medium.destroy();
-
-      this.showMediaView();
-    },
-
-    Library.prototype.cancelMediaDeletion = function() {
-      this.showMediaView(this.selectedMedium);
-    }
+    });
 
     return Library;
   }
